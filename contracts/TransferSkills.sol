aaf6aaa38efc6fdf4c96e7f42429b30f63e70b30
José Angel Contreras Gedler <contrerasgedler@gmail.com>
2019-10-20 19:56:23 -0400

    New functions and the events to the main contract

--- contracts/TransferSkills.sol
+++ contracts/TransferSkills.sol
@@ -1,9 +1,10 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-
 contract TransferSkills {
 
-// fd
+  event Message(address indexed _sender, address indexed _receiver, uint256 _time, string message);
+  event PublicKeyUpdated(address indexed _sender, string _key, string _keytype);
+
   mapping (address => string) message;
   mapping (address => string) file;
 
@@ -15,8 +16,7 @@ contract TransferSkills {
     return message[msg.sender];
   }
 
-
-   function sendIPFSFile(address  _recipient, string memory  _file) public {
+  function sendIPFSFile(address  _recipient, string memory  _file) public {
     file[_recipient] = _file;
   }
 
@@ -24,4 +24,19 @@ contract TransferSkills {
     return file[msg.sender];
   }
 
+  function lastIndex(address _owner) constant returns (uint256) {
+      return last_msg_index[_owner];
+  }
+    
+  function getLastMessage(address _who) constant returns (address, string, uint256) {
+    require(last_msg_index[_who] > 0);
+    return (messages[_who][last_msg_index[_who] - 1].from, messages[_who][last_msg_index[_who] - 1].text, messages[_who][last_msg_index[_who] - 1].time);
+  }
+    
+  function getMessageByIndex(address _who, uint256 _index) constant returns (address, string, uint256) {
+
+    return (messages[_who][_index - 1].from, messages[_who][_index - 1].text, messages[_who][_index - 1].time);
+  }
+    
 }
+
